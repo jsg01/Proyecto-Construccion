38c0f1bb6182798548a2787ff53bc74626635dcc
ricardo <ricardo.ortizpozo@luxpmsoft.com>
2026-03-24 23:15:37 +0900

    some new files

---
+++ calibration_package/calibration_package/analysis_threshold.py
@@ -0,0 +1,66 @@
+import numpy as np
+import cv2
+
+def compute_thresholds(rgi: np.ndarray, roi: tuple, lower_pct: int, upper_pct: int):
+    """
+    This function outputs a dictionary with the max and minimum thresholds for detecting color
+    
+    Args:
+        rgi (np.ndarray): The image where the cropping will be performed, in RGI format.
+        roi (tuple): A tuple with the coordinates of the upper left corner of a piece, its width and its height.
+        lower_pct (int): The lower percentile of the threshold
+        upper_pct (int): The upper percentile of the threshold
+
+    Returns:
+        thresholds (dict): {R_min, R_max, G_min, G_max, mass}
+    """
+
+    x, y, w, h = roi
+    patch = rgi[y:y+h, x:x+w]
+    R = patch[:, :, 0].reshape(-1)
+    G = patch[:, :, 1].reshape(-1)
+    thresholds = {
+        "R_min": float(np.percentile(R, lower_pct)),
+        "R_max": float(np.percentile(R, upper_pct)),
+        "G_min": float(np.percentile(G, lower_pct)),
+        "G_max": float(np.percentile(G, upper_pct)),
+        "mass": w * h
+    }
+    return thresholds
+
+def classify_pixels(rgi, thresholds):
+    """
+    Creates a mask with the pixels that are in between the thresholds
+    Args:
+        rgi(np.ndarray): Image in RGI format
+        thresholds(dict): A dictionary with the thresholds for color classification
+    Returns:
+        mask(np.ndarray): A boolean matrix with True value for pixels within boundaries
+        and False for everything else 
+    """
+
+    R = rgi[:, :, 0]
+    G = rgi[:, :, 1]
+
+    mask = (R >= thresholds["R_min"] and 
+            R <= thresholds["R_max"] and 
+            G >= thresholds["G_min"] and 
+            G <= thresholds["G_max"])
+    return mask
+
+def select_roi(image: np.ndarray):
+    """
+    Lets the user draw a rectangle on the image.
+    
+    Args:
+        image(np.array): Image where to get the ROI
+    Returns:
+        (x, y, w, h) (int) x, y coordinates for the upper left corner and the width and height of the block 
+    """
+    
+    roi = cv2.selectROI("Select ROI", image, showCrosshair=True, fromCenter=False)
+    x, y, w, h = roi
+    print(f"Coordinates gotten: x={x}, y={y}, w={w}, h={h}")
+    cv2.destroyWindow("Select ROI")
+    cv2.waitKey(1)
+    return int(x), int(y), int(w), int(h)
---
+++ detection_node/detection_node/piece_detection_node_second.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python3
+import math
+import cv2
+import numpy as np
+import rclpy
+from rclpy.node import Node
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
+from . import helper as hp
+
+
+class PieceDetectionNode(Node):
+    def __init__(self):
+        """
+        Initializes the node for handling an image
+        """
+
+        super().__init__('detection_node')
+        self.opencv_bridge_ = CvBridge()
+        self.subscriber_setup()
+        self.publisher_setup()
+        self.background_threshold = 12 #*This value changes manually
+        self.get_logger().info("Detection node fully setup")
+
+    def publisher_setup(self):
+        """
+        Initializes all the publishers in the node
+        """
+
+        self.image_processed_publisher = self.create_publisher(Image, "/image_raw_processed", 10)
+    
+    def subscriber_setup(self):
+         """
+         Initializes all the subscribers in the node
+         """
+
+         self.unprocessed_image_subscription = self.create_subscription(Image, "/image_raw", self.image_processor_callback, 10)
+    
+    def image_processor_callback(self, msg: Image):
+        """
+        Processes the image gotten as an Image message and publishes the processed image back
+
+        ARGS:
+            msg (Image): The message received.
+        """
+
+        try:
+            self.current_bgr = self.opencv_bridge_.imgmsg_to_cv2(msg, desired_encoding='passthrough')
+            self.current_bgr = cv2.cvtColor(self.current_bgr, cv2.COLOR_YUV2BGR_YUY2)
+        except CvBridgeError as e:
+            self.get_logger().error('Image sensor msg to Opencv conversion is not possible : {}'.format(e))
+            return
+        mask = (self.current_bgr == 255).any(axis = 2)
+        self.current_bgr[mask] = 0
+        self.current_bgr, _ = hp.BGR2RGI(self.current_bgr)
+        #self.current_bgr, _ = hp.foreground_from_rgi(self.current_bgr, _)
+        try:
+            msg_out = self.opencv_bridge_.cv2_to_imgmsg(_, encoding='bgr8')
+        except CvBridgeError as e:
+            self.get_logger().error(f'Publish conversion failed: {e}')
+            return
+        self.image_processed_publisher.publish(msg_out)
+        
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = PieceDetectionNode()
+    try:
+        rclpy.spin(node)
+    except KeyboardInterrupt:
+        pass
+    finally:
+        cv2.destroyAllWindows()
+        node.destroy_node()
+        rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
+
+
